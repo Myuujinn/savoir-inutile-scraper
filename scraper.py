0cc24a98aa8dc2225962d6d91b4add868f8026f9
Victor LE DANTEC <victor.le-dantec@epitech.eu>
2018-08-12 22:55:32 +0200

    Scrape all

--- scraper.py
+++ scraper.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python3
+import sys
 import urllib.request
 import bs4
 
 url = "https://www.savoir-inutile.com/"
+file_name = "phrases.txt"
 
-page = urllib.request.urlopen(url)
-soup = bs4.BeautifulSoup(page, "html.parser")
-phrase = soup.find("h2", {"id": "phrase"})
+def scrape():
+    page = urllib.request.urlopen(url)
+    soup = bs4.BeautifulSoup(page, "html.parser")
+    phrase = soup.find("h2", {"id": "phrase"})
 
-print(phrase.text)
+    return phrase.text
+
+
+def scrape_all():
+    phrase_list = []
+    while len(phrase_list) < 500:
+        phrase = scrape()
+        print("Scraped : ", phrase[:50], "..", sep='')
+        if phrase not in phrase_list:
+            phrase_list.append(phrase)
+
+    print("Writing", file_name)
+    file = open(file_name, "w+")
+    for phrase in phrase_list:
+        file.write(phrase + "\n")
+    file.close()
+
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == "--scrape-all":
+        scrape_all()
+    else:
+        print(scrape())
+
+
+if __name__ == '__main__':
+    main()
